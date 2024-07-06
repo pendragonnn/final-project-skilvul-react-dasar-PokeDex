99ce7d73f9869fb9a11d5f139ced0d3f1f440746
Wisnu Andika <124800648+pendragonnn@users.noreply.github.com>
2024-07-06 13:49:31 +0700

    feat: add search pokemon

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -5,12 +5,32 @@ import PokemonItem from '../PokemonItem/PokemonItem'
 
 const PokemonList = () => {
   const [pokemons] = useState(pokemonJson)
+  const [filterPokemons, setFilterPokemons] = useState(pokemonJson)
+
+  const handleSearch = (e) => {
+    let search = pokemons.filter((item) => {
+      return item.name.toLowerCase().includes(e.target.value)
+    })
+
+    setFilterPokemons(search)
+  }
+
   return (
     <div>
+      <input 
+        type="text"
+        placeholder='cari pokemon...'
+        className='search'
+        onChange={handleSearch}
+      />
       <div className='list-pokemon'>
-        {pokemons.map((item) => (
-          <PokemonItem key={item.id} pokemon={item} />
-        ))}
+        {filterPokemons.length == 0 ? (
+          <div>Data tidak ditemukan</div>
+        ) : (
+          filterPokemons.map((item) => (
+            <PokemonItem key={item.id} pokemon={item} />
+          ))
+        )}
       </div>
     </div>
   )
